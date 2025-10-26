917364929f27310a2d957d5f5fb45110b0951ea7
Maldifassi <maldifassi.riccardo@iisgalvanimi.edu.it>
2025-10-26 15:41:07 +0000

    fatti es 4, 1, 2, 3

--- esercizi/src/app/app.ts
+++ esercizi/src/app/app.ts
@@ -1,33 +1,39 @@
-import { Component, signal,  } from '@angular/core';
-import { Componente1 } from './componente1/componente1'
+import { Component, signal } from '@angular/core';
+import { Componente1 } from './componente1/componente1';
 import { Esercizio1 } from './esercizio1/esercizio1';
 import { Esercizio2 } from './esercizio2/esercizio2';
+import { Esercizio3 } from './esercizio3/esercizio3';
 import { CommonModule } from '@angular/common';
 import { Post } from './post/post';
-import { Esercizio3 } from './esercizio3/esercizio3';
 
+interface PostType {
+  titolo: string;
+  testo: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [Componente1, Esercizio1, Esercizio2, CommonModule, Post, Esercizio3],
+  imports: [CommonModule, Componente1, Esercizio1, Esercizio2, Esercizio3, Post],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  messaggio = 'ciao dal componente padre'
+  messaggio = 'Ciao da componente padre'
 
-  posts = [
-  {
-    titolo: 'titolo1',
-    testo: 'esempio di testo1'
-  },
-  {
-    titolo: 'titolo2',
-    testo: 'esempio di testo2'
-  },
-  {
-    titolo: 'titolo3',
-    testo: 'esempio di testo3'
-  }
+  posts: PostType[] = [
+    {
+      titolo: 'titolo1',
+      testo: 'esempio di testo1'
+    },
+    {
+      titolo: 'titolo1',
+      testo: 'esempio di testo2'
+    },
+    {
+      titolo: 'titolo3',
+      testo: 'esempio di testo maggiore a venti caratteriiiiiiiiiiiiiiiii'
+    }
   ];
-}
+  get postVisibili() {
+    return this.posts.filter(p => p.testo.length < 20);
+  }
--- esercizi/src/app/post/post.ts
+++ esercizi/src/app/post/post.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post.html',
   styleUrl: './post.css'
 })
 export class Post {
- @Input() titolo!: string;
- @Input() testo!: string;
+  @Input() titolo!: string;
+  @Input() testo!: string;
 }
